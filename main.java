406b3c333f473734920d51d502825c2dec669ab2
Svetikyss <82896134+Svetikyss@users.noreply.github.com>
2023-09-26 16:29:12 -0700

    "add food and rooms"

---
+++ main.java
@@ -0,0 +1,52 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+class Food implements Serializable
+{
+    int itemno;
+    int quantity;   
+    float price;
+    
+    Food(int itemno,int quantity)
+    {
+        this.itemno=itemno;
+        this.quantity=quantity;
+        switch(itemno)
+        {
+            case 1:price=quantity*50;
+                break;
+            case 2:price=quantity*60;
+                break;
+            case 3:price=quantity*70;
+                break;
+            case 4:price=quantity*30;
+                break;
+        }
+    }
+}
+class Singleroom implements Serializable
+{
+    String name;
+    String contact;
+    String gender;   
+    ArrayList<Food> food =new ArrayList<>();
+
+   
+    Singleroom()
+    {
+        this.name="";
+    }
+    Singleroom(String name,String contact,String gender)
+    {
+        this.name=name;
+        this.contact=contact;
+        this.gender=gender;
+    }
+}
