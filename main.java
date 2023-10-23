925c078185c895260569c47f1da6d9f0e89d9e7d
Svetikyss <82896134+Svetikyss@users.noreply.github.com>
2023-10-22 19:44:21 -0700

    Doubleroom

--- main.java
+++ main.java
@@ -50,3 +50,79 @@ class Singleroom implements Serializable
         this.gender=gender;
     }
 }
+class Doubleroom extends Singleroom implements Serializable
+{ 
+    String name2;
+    String contact2;
+    String gender2;  
+    
+    Doubleroom()
+    {
+        this.name="";
+        this.name2="";
+    }
+    Doubleroom(String name,String contact,String gender,String name2,String contact2,String gender2)
+    {
+        this.name=name;
+        this.contact=contact;
+        this.gender=gender;
+        this.name2=name2;
+        this.contact2=contact2;
+        this.gender2=gender2;
+    }
+}
+class NotAvailable extends Exception
+{
+    @Override
+    public String toString()
+    {
+        return "Not Available !";
+    }
+}
+
+class holder implements Serializable
+{
+    Doubleroom luxury_doublerrom[]=new Doubleroom[10]; //Luxury
+    Doubleroom deluxe_doublerrom[]=new Doubleroom[20]; //Deluxe
+    Singleroom luxury_singleerrom[]=new Singleroom[10]; //Luxury
+    Singleroom deluxe_singleerrom[]=new Singleroom[20]; //Deluxe
+}
+
+class Hotel
+{
+    static holder hotel_ob=new holder();
+    static Scanner sc = new Scanner(System.in);
+    static void CustDetails(int i,int rn)
+    {
+        String name, contact, gender;
+        String name2 = null, contact2 = null; 
+        String gender2="";
+        System.out.print("\nEnter customer name: ");
+        name = sc.next();
+        System.out.print("Enter contact number: ");
+        contact=sc.next();
+        System.out.print("Enter gender: ");
+        gender = sc.next();
+        if(i<3)
+        {
+        System.out.print("Enter second customer name: ");
+        name2 = sc.next();
+        System.out.print("Enter contact number: ");
+        contact2=sc.next();
+        System.out.print("Enter gender: ");
+        gender2 = sc.next();
+        }      
+        
+          switch (i) {
+            case 1:hotel_ob.luxury_doublerrom[rn]=new Doubleroom(name,contact,gender,name2,contact2,gender2);
+                break;
+            case 2:hotel_ob.deluxe_doublerrom[rn]=new Doubleroom(name,contact,gender,name2,contact2,gender2);
+                break;
+            case 3:hotel_ob.luxury_singleerrom[rn]=new Singleroom(name,contact,gender);
+                break;
+            case 4:hotel_ob.deluxe_singleerrom[rn]=new Singleroom(name,contact,gender);
+                break;
+            default:System.out.println("Wrong option");
+                break;
+        }
+    }
